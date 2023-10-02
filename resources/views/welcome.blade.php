201d6cdf8221e0b29f4e231c09b0108227e6a065
Anderson Rocha <32132934+andersondesigner3d@users.noreply.github.com>
2023-10-02 09:52:37 -0300

    primeiro commit - liveware instalado

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estudos de Laravel 10, PHP 8.2 com Liveware 3.0</title>
+    @livewireStyles
+</head>
+<body>
+    <h1>Laravel 10 - PHP 8.2 - Liveware 3.0</h1>
+    
+@livewireScripts
+</body>
+</html>
